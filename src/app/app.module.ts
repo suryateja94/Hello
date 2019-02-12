39858233bc4a60a93236e9cc1ade8baa50611ae5
suryatejasharma <sharmasuryateja@gmail.com>
2019-02-12 09:22:09 -0500

    Storing into db and extracting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignupComponent } from './signup/signup.component';
 
+import { IonicStorageModule } from '@ionic/storage';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [AppComponent, SignupComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), FormsModule ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
 //import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Storage } from '@ionic/storage';
+import { PersonalProfile, WorkProfile } from '../profile';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+
+
+export class SignupComponent implements OnInit {
+
+  public work_profile : WorkProfile;
+
+  public personal_profile : PersonalProfile;
 
   public personal : number;
 
@@ -14,13 +23,34 @@ export class SignupComponent {
 
   public profile : string;
 
-  constructor(){
+
+  setInputFields(){
+
+
+      let promise = this.storage.get("personal_profile");
+      promise.then(data => {
+        this.personal_profile = JSON.parse(data);
+      });
+
+
+      let work_promise = this.storage.get("work_profile");
+      work_promise.then(data => {
+        this.work_profile = JSON.parse(data);
+      });
+
+  }
+
+  constructor(private storage: Storage){
     this.personal = 1;
     this.profile = 'Personal';
     this.count = 0;
+    this.personal_profile = new PersonalProfile();
+    this.work_profile = new WorkProfile();
    }
 
-
+   ngOnInit() {
+     this.setInputFields();
+   }
 
    segmentChanged(ev: any){
      if(this.count==1){
@@ -34,6 +64,20 @@ export class SignupComponent {
     else{
       this.count = 1;
     }
+   }
+
+   savePeronalProfile(){
+     if(this.personal_profile){
+       this.storage.set("personal_profile",JSON.stringify(this.personal_profile));
+     }
+   }
+
+   saveWorkProfile(){
+     if(this.work_profile){
+       console.log("Hits");
+       this.storage.set("work_profile",JSON.stringify(this.work_profile));
+
+     }
    }
 
 
